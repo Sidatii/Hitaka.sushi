025ac42639f59accd2c2483be0805c0169053d99
mohammedaitgaba <93977101+mohammedaitgaba@users.noreply.github.com>
2024-05-05 22:57:56 +0100

    add cart ui

--- javascript/script.js
+++ javascript/script.js
@@ -202,5 +202,14 @@ function submitConfirmation(){
       });
 }
 
+function openCart(){
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
 
         
